0594f7fcfee42142112e7b03ed41194df9300dab
Sebastian Hirnschall <sebastian@hirnschall.net>
2023-09-28 21:04:02 +0200

    testing fan speed reset issues

--- src/homekit/accessories/HttpWebHookFanv2Accessory.js
+++ src/homekit/accessories/HttpWebHookFanv2Accessory.js
@@ -69,6 +69,8 @@ function HttpWebHookFanv2Accessory(ServiceParam, CharacteristicParam, platform, 
     this.modelPrefix = fanv2Config["modelPrefix"] || "HttpWebHookAccessory-";
     this.serialPrefix = fanv2Config["serialPrefix"] || "HttpWebHookAccessory-";
 
+    this.speed = 100;       //issues when more than one fan is used..
+
     this.informationService = new Service.AccessoryInformation();
     this.informationService.setCharacteristic(Characteristic.Manufacturer, this.manufacturer);
     this.informationService.setCharacteristic(Characteristic.Model, this.modelPrefix + this.name);
@@ -104,7 +106,7 @@ HttpWebHookFanv2Accessory.prototype.changeFromServer = function (urlParams) {
         this.service.getCharacteristic(Characteristic.Active).updateValue((urlParams.state == "true"), undefined, Constants.CONTEXT_FROM_WEBHOOK);
     }
     if (urlParams.speed != null) {
-        var cachedSpeed = this.storage.getItemSync("http-webhook-speed-" + this.id);
+        var cachedSpeed = this.speed;
         var speed = parseInt(urlParams.speed);
         if (cachedSpeed != speed) {
             this.log("Change speed for fanv2 to '%d'.", speed);
@@ -183,7 +185,7 @@ HttpWebHookFanv2Accessory.prototype.getSpeed = function (callback) {
     }
     var speed = 0;
     if (state) {
-        speed = this.storage.getItemSync("http-webhook-speed-" + this.id);
+        speed = this.speed;
         if (speed === undefined) {
             speed = 100;
         }
@@ -196,6 +198,8 @@ HttpWebHookFanv2Accessory.prototype.setSpeed = function (speed, callback, contex
     var newState = speed > 0;
     this.storage.setItemSync("http-webhook-" + this.id, newState);
     this.storage.setItemSync("http-webhook-speed-" + this.id, speed);
+    this.speed=speed;
+    this.log("cached speed set to %d", this.speed);
     var speedFactor = this.speedFactor;
     var speedToSet = Math.ceil(speed * speedFactor);
     var urlToCall = this.replaceVariables(this.speedURL, newState, speedToSet);
